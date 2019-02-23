b006d4c8aeb3e236467281d8f311b543b723e3eb
Luppolo <ololko@github.com>
2019-02-23 20:03:01 +0200

    Now I will try to start non blocling

--- server.py
+++ server.py
@@ -4,13 +4,10 @@ import re
 
 
 def splitRequest(data):
-    data = data.splitlines()
-    print 'DATA: %s' %data
+    return data.splitlines()
     return data
 
-def removeSpaces(line):
-    print 'Linia pred %s' %line
-    print 'Linia je rozdelena na %s' % re.split("/",line)
+def getRequestType(line):
     line = re.split("/",line)
     return line[0].replace(" ", "")
 
@@ -18,10 +15,9 @@ def removeSpaces(line):
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = int(sys.argv[1])
 
-#sock.bind = (socket.gethostname(), port )
-print 'starting up on localhost port %s' %  port
 try:
-    sock.bind(('', port))
+    #sock.bind = ((socket.gethostname(), port) )
+    sock.bind( ('', port) )
 except:
     print 'Port is taken'
     sys.exit()
@@ -31,54 +27,38 @@ sock.listen(100)
 while True:
     print 'waiting for a connection'
     connection, client_address = sock.accept()
-    print 'connection from', client_address
 
     data = connection.recv(1024)
+    print data
     data = splitRequest(data)
-    requestType = removeSpaces(data[0])
+    requestType = getRequestType(data[0])
 
-    if requestType == "GET":
-        '''
+    if requestType != "GET":
+        response_proto = 'HTTP/1.1'
+        response_status = '405 '
+        response_status_text = 'Method Not Allowed' # this can be random
+        connection.send('%s %s %s\r\n' % (response_proto, response_status, \
+                                                        response_status_text))
+        connection.send('\r\n')
+    else:     
         response_proto = 'HTTP/1.1'
         response_status = '200'
-        response_status_text = 'OK' # this can be random
+        response_status_text = 'OK'
+
+        msg = 'Spravne'
 
         response_headers = {
             'Content-Type': 'text/html; encoding=utf8',
-            'Connection': 'close',
+            'Content-Length': len(msg),
         }
         response_headers_raw = ''.join('%s: %s\n' % (k, v) for k, v in \
                                                 response_headers.iteritems())
 
-        # sending all this stuff
-        connection.send('%s %s %s' % (response_proto, response_status, \
+        connection.send('%s %s %s\r\n' % (response_proto, response_status, \
                                                         response_status_text))
         connection.send(response_headers_raw)
-        print "Je to GET miamor!"
-        '''
-        response_proto = 'HTTP/1.1'
-        response_status = '200'
-        response_status_text = 'SKUSKA' # this can be random
-
-        response_headers = {
-            'Content-Type': 'text/html; encoding=utf8',
-            'Content-Length': len('ODPOVED'),
-        }
-        response_headers_raw = ''.join('%s: %s\n' % (k, v) for k, v in \
-                                                response_headers.iteritems())
-
-        connection.send('%s %s %s' % (response_proto, response_status, \
-                                                        response_status_text))
-        connection.send(response_headers_raw)
-        connection.send('\n') # to separate headers from body
-        connection.send('ODPOVED')
-    else:
-        response_proto = 'HTTP/1.1'
-        response_status = '400'
-        response_status_text = 'BAD' # this can be random
-        connection.send('%s %s %s' % (response_proto, response_status, \
-                                                        response_status_text))
-        print "Nauc vratit chybnu hlasku"
+        connection.send('\r\n') # to separate headers from body
+        connection.send(msg)
 
     
     connection.close()
